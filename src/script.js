cb803f26394531b679ef76d643d35b40c9ec5e12
alfre <alfrerodriguez01@gmail.com>
2021-10-15 16:20:05 -0500

    set tech section

--- src/script.js
+++ src/script.js
@@ -1,7 +1,34 @@
 import './style/main.scss'
 
-const sayHello = () =>{
-    return "demo del git clone"
-}
+const images = [
+    "js-square.svg",
+    "angular.svg",
+    "reactjs.svg",
+    "nodejs.svg",
+    "html-5.svg",
+    "css3.png",
+    "git.svg",
+    "python.svg",
+    "npm.svg",
+    "php.svg",
+    "c_logo.svg",
+    "mysql.svg",
+    "os-linux.png",
+    "bootstrap.svg",
+    "sass.png",
+    "stylus.svg",
+    "webpack.svg"
+]
+let arrayImage = [];
+images.map(item => {
+    const img = document.createElement("img")
+    img.src = `./assets/images/${item}`
+    img.alt = item
+    img.className = "tech-container__img"
+    arrayImage.push(img)
+})
+console.log(arrayImage)
+const tech = document.querySelector(".tech-container")
+tech.append(...arrayImage)
 
 console.log(sayHello());
